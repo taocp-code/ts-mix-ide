c204d00c7d01a31e3b17e0c75e058a87ec5be766
Luke <luke.the.coder@gmail.com>
2026-03-26 14:54:43 +1100

    MIX word load and store.

---
+++ src/mix/data-types.ts
@@ -0,0 +1,131 @@
+/**
+ * Compare results.
+ */
+export enum Compare {
+    EQUAL = 0,
+    LESS = -1,
+    GREATER = 1,
+}
+
+/**
+ * A byte in MIX word.
+ */
+export type MixByte = number;
+
+// maximum number of states a MIX byte can represent.
+export const MIX_BYTE_MAX = 100;
+// Number of bytes in a MIX word, excluding the sign.
+export const MIX_WORD_SIZE = 5;
+export const SIGN_POSITIVE = 1;
+export const SIGN_NEGATIVE = -1;
+
+export const F_ALL = _mix_field_encode(0, MIX_WORD_SIZE);
+export const F_OP_ADDR = _mix_field_encode(0, 2);
+export const F_OP_I = _mix_field_encode(3, 3);
+export const F_OP_F = _mix_field_encode(4, 4);
+export const F_OP_CODE = _mix_field_encode(5, 5);
+
+/**
+ * Represent a field range in MIX word, inclusive [L, R]
+ */
+export interface MixField {
+    l: number;
+    r: number;
+}
+export function _mix_field_decode(f: number): MixField {
+    return {
+        l: Math.floor(f / 8),
+        r: f % 8,
+    }
+}
+export function _mix_field_encode(l: number, r: number) {
+    return l * 8 + r;
+}
+
+function _sign_of(v: number) {
+    return v >= 0 ? SIGN_POSITIVE : SIGN_NEGATIVE;
+}
+
+/**
+ * A MIX word, which is a 5-byte value with sign.
+ * Word layout:
+ * Index:   0     1    2    3    4    5
+ *       | +/- | B1 | B2 | B3 | B4 | B5 |
+ * Where B1 is the most significant byte, B5 is the least significant byte.
+ */
+export class MixWord {
+    public static readonly ZERO = new MixWord(0);
+    private static readonly MAX_VALUE = Math.pow(MIX_BYTE_MAX, MIX_WORD_SIZE) - 1;
+    // array of bytes, including the sign byte.
+    private _bytes: MixByte[];
+
+    constructor(value: number = 0) {
+        // bytes[0] is sign, bytes[1] is the most significant byte, bytes[size] is the least significant byte.
+        this._bytes = new Array(MIX_WORD_SIZE + 1).fill(0);
+        this._bytes[0] = _sign_of(value);
+        this._setAbsValue(value);
+    }
+
+    get sign() {
+        return this._bytes[0];
+    }
+    set sign(s: number) {
+        this._bytes[0] = _sign_of(s);
+    }
+    get abs() {
+        let x = 0;
+        for (let i = 0; i < MIX_WORD_SIZE; i++) {
+            x = x * MIX_BYTE_MAX + this._bytes[MIX_WORD_SIZE - i];
+        }
+        return x;
+    }
+    set abs(v: number) {
+        this._setAbsValue(v);
+    }
+    get value() {
+        return this.sign * this.abs;
+    }
+    set value(v: number) {
+        this.sign = _sign_of(v);
+        this._setAbsValue(v);
+    }
+
+    /**
+     * Load a MIX word from the field range.
+     * @param f
+     */
+    load(f: number): MixWord {
+        const {l, r} = _mix_field_decode(f);
+        const s = l == 0 ? this.sign : SIGN_POSITIVE;
+        let x = 0;
+        for (let i = r; i >= Math.max(l, 1); i--) {
+            x = x * MIX_BYTE_MAX + this._bytes[i];
+        }
+        return new MixWord(s * x);
+    }
+
+    /**
+     * Store a value into the MIX word, update bytes based on the field range.
+     *
+     * f specifies a range [L, R] and the current instance's bytes[L] to bytes[R] are updated.
+     * If R is less than the word size, the val is "shifted" to the left.
+     *
+     * @param val the MIX word to store.
+     * @param f field descriptor.
+     */
+    store(val: MixWord, f: number = F_ALL) {
+        const {l, r} = _mix_field_decode(f);
+        for (let i = r - l; i >= 0; i--) {
+            this._bytes[l + i] = val._bytes[MIX_WORD_SIZE - r + i];
+        }
+    }
+
+    private _setAbsValue(v: number) {
+        v = Math.abs(v);
+        if (v > MixWord.MAX_VALUE) throw new Error(`Value too large: ${v}.`);
+        for (let i = 0; i < MIX_WORD_SIZE; i++) {
+            this._bytes[MIX_WORD_SIZE - i] = v % MIX_BYTE_MAX;
+            v = Math.floor(v / MIX_BYTE_MAX);
+        }
+    }
+}
---
+++ src/mix/mix.ts
@@ -0,0 +1,147 @@
+import {Compare, F_ALL, F_OP_ADDR, F_OP_CODE, F_OP_F, F_OP_I, MixWord} from "./data-types.ts";
+
+/**
+ * Decoded MIX op from a MIX word.
+ */
+interface MixOp {
+    // OP code
+    c: number;
+    // Field desc, usually 8 * L + R or other meanings.
+    f: number;
+    // Index register, 0-6
+    i: number;
+    // Address value, sign and bytes 1-3
+    a: MixWord;
+}
+
+/**
+ * OP code table
+ */
+const OP_LDA = 8;
+const OP_STA = 9;
+
+function decodeOp(word: MixWord): MixOp {
+    return {
+        c: word.load(F_OP_CODE).value,
+        f: word.load(F_OP_F).value,
+        i: word.load(F_OP_I).value,
+        a: word.load(F_OP_ADDR),
+    }
+}
+
+
+class MixMemory {
+    public static readonly SIZE = 4000;
+    private words: MixWord[] = new Array(MixMemory.SIZE).map(() => new MixWord());
+
+    load(addr: number, f: number=F_ALL): MixWord {
+        return this.words[addr].load(f);
+    }
+
+    store(addr: number, value: MixWord, f: number=F_ALL) {
+        this.words[addr].store(value, f);
+    }
+
+    reset() {
+        this.words.forEach(w => w.store(MixWord.ZERO));
+    }
+}
+
+export class Mix {
+    private _memory: MixMemory = new MixMemory();
+    private _rA: MixWord = new MixWord();
+    private _rX: MixWord = new MixWord();
+    private _rI: MixWord[] = [
+        new MixWord(),
+        new MixWord(),
+        new MixWord(),
+        new MixWord(),
+        new MixWord(),
+        new MixWord()
+    ];
+    private _rJ: MixWord = new MixWord();
+    private _overflow: boolean = false;
+    private _compare: Compare = Compare.EQUAL;
+    private _pc: number = 0;
+
+    constructor() {
+        this.reset();
+    }
+
+    reset() {
+        this._memory.reset();
+        this._rA.store(MixWord.ZERO);
+        this._rX.store(MixWord.ZERO);
+        this._rI.forEach(r => r.store(MixWord.ZERO));
+        this._rJ.store(MixWord.ZERO);
+        this._overflow = false;
+        this._compare = Compare.EQUAL;
+        this._pc = 0;
+    }
+
+    step() {
+        const op = decodeOp(this._memory.load(this._pc));
+        this._pc++;
+        switch (op.c) {
+            case OP_LDA:
+                this.lda(op.f, op.i, op.a);
+                break;
+            case OP_STA:
+                this.sta(op.f, op.i, op.a);
+                break;
+        }
+    }
+
+    get overflow() {
+        return this._overflow;
+    }
+    get compare() {
+        return this._compare;
+    }
+    get rA() {
+        return this._rA;
+    }
+    get rX() {
+        return this._rX;
+    }
+    get rI1() {
+        return this._rI[0];
+    }
+    get rI3() {
+        return this._rI[2];
+    }
+
+    get rI4() {
+        return this._rI[3];
+    }
+
+    get rI5() {
+        return this._rI[4];
+    }
+    get rI6() {
+        return this._rI[5];
+    }
+
+    private getI(i: number) {
+        if (i == 0) return MixWord.ZERO;
+        if (i < 6) return this._rI[i-1];
+        return undefined;
+    }
+
+    private getM(i: number, a: MixWord): number {
+        const rI = this.getI(i);
+        if (!rI) throw new Error(`Invalid index register: ${i}.`);
+        return rI.value + a.value;
+    }
+
+    private lda(f: number, i: number, a: MixWord) {
+        const M = this.getM(i, a);
+        const content = this._memory.load(M, f);
+        this._rA.store(content);
+    }
+
+    private sta(f: number, i: number, a: MixWord) {
+        const M = this.getM(i, a);
+        this._memory.store(M, this._rA, f);
+    }
+}
