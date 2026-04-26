c27413dd558ab41cf30d4d51632880a5c3454915
Luke <luke.the.coder@gmail.com>
2026-04-26 19:08:14 +1000

    Add parser generator

--- src/mixal/mix-parser.js
+++ src/mixal/mix-parser.js
@@ -1,13 +1,24 @@
 import * as fs from 'fs/promises';
 import {parser} from "./parser.js";
 
-fs.readFile('public/examples/halt-fill.ms').then(value => {
+fs.readFile('public/examples/coroutine-decode.ms').then(value => {
     const text = value.toString();
     const program = parser.parse(value.toString());
     const cur = program.cursor();
+    let indent = '';
     cur.iterate((n) => {
-        console.log('Entering', n.name, n.type.isError, text.substring(n.from, n.to));
+
+        if (n.name === 'Comment' || n.name === 'LineComment') {
+            if (n.from < n.to) {
+                console.log(`${indent}Ent ${n.name} ${text.substring(n.from, n.to).trim()}`)
+            }
+        }
+        if (n.type.isError) {
+            console.log(`${indent}!Error `, n.name, n.type.isError, text.substring(n.from, n.to));
+        }
+        indent = indent + '    ';
     }, (n) => {
-        console.log('Leaving', n.name);
+        indent = indent.slice(0, indent.length - 4);
+        // console.log(`${indent}Lea ${n.name}`)
     });
 })
